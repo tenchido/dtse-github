e8879b94d0db9d75f72cf6cab8dd81e64e31307e
Darren Tse <darren.tse14@gmail.com>
2017-09-20 23:45:30 -0300

    Exercise 9

---
+++ Exercise9.java
@@ -0,0 +1,18 @@
+/*CSCI 1100-Lab 1 Report-Exercise 9
+   This program will print the text given in exercise 9.
+   Darren Tse, B00780687, Sept 20,2017*/
+   
+public class Exercise9 {
+   public static void main(String[] args) {
+   //The following codes will print out the drawing from exercise 9.
+      //Each line of code prints one line of the drawing.
+      System.out.println("     /\\"); 
+      System.out.println("    /  \\");
+      System.out.println("   //  \\\\");
+      System.out.println("   \"\"\"\"\"\"");
+      System.out.println("     ||");
+      System.out.println("  --------");
+      System.out.println("  \\\\    //");
+      System.out.println("************");
+   }
+}
